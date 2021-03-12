c8dd07d7e94e395da3151bcecef20a3dab58c949
byrontung <btung1@ualberta.ca>
2021-03-12 02:24:47 -0700

    Combined Kenny's work with mine.

--- experimently/app/src/main/java/com/example/experimentify/Experiment.java
+++ experimently/app/src/main/java/com/example/experimentify/Experiment.java
@@ -3,8 +3,6 @@ package com.example.experimentify;
 
 import android.media.Image;
 
-
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -24,6 +22,7 @@ public class Experiment implements Serializable {
     private String region;
     private boolean ended;
     private String uid;
+    private boolean editable;
 
 
     public Experiment(String description, String region, long minTrials, String date, boolean locationRequired) {
@@ -32,6 +31,7 @@ public class Experiment implements Serializable {
         this.region = region;
         this.minTrials = minTrials;
         this.locationRequired = locationRequired;
+        editable = true;
         ended = false;
         viewable = true;
     }
@@ -75,11 +75,22 @@ public class Experiment implements Serializable {
     public boolean isEnded() {
         return ended;
     }
-    public String getUID(){return uid;}
 
-    public void setUID(String id){this.uid = id;}
+    public String getUID() {
+        return uid;
+    }
 
+    public void setUID(String id) {
+        this.uid = id;
+    }
 
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
 
     //TODO Ask about the variables below
     //private Location region //Region? - from requirements
