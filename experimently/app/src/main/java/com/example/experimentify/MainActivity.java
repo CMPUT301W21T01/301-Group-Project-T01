682e09088cd6e4baa98f8f7c707e9bc45b9490d7
Riley Ward <riley4242@hotmail.com>
2021-03-12 23:15:33 -0700

    Add database logic for user profile to MainActivity, initialize user object
    90 min 100% completion speedrun WR

--- experimently/app/src/main/java/com/example/experimentify/MainActivity.java
+++ experimently/app/src/main/java/com/example/experimentify/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.experimentify;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -19,7 +27,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -34,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements AddExpFragment.On
     private FloatingActionButton userProfileButton;
     private ArrayList<Experiment> experimentList;
     final String TAG = MainActivity.class.getName();
+    public static final String PREFS_NAME = "PrefsFile";
     FirebaseFirestore db;
 
     /**
@@ -75,6 +85,10 @@ public class MainActivity extends AppCompatActivity implements AddExpFragment.On
 
         db = FirebaseFirestore.getInstance();
         final CollectionReference collectionReference = db.collection("Experiments");
+
+        User user = initializeUser(db);
+
+
 
         exListView = findViewById(R.id.exListView);
         showAddExpUiButton = findViewById(R.id.showAddExpUiButton);
@@ -141,8 +155,110 @@ public class MainActivity extends AppCompatActivity implements AddExpFragment.On
     public void onOkPressed(Experiment newExp, Boolean edit) {
 
     }
+    /**
+     * initialize a new user to the firestore, we do not force them to provide info like name, email, username, e.t.c until later, we are just make a document with a unique ID that can identify that specific user
+     * @param db is the firestore db
+     */
+    public User initializeUser(FirebaseFirestore db){
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
+        User user = new User();
+        if(settings.contains("uid")){
+            String localUID = settings.getString("uid", "0");
 
-    //Work in progress for deleting an experiment.
+            DocumentReference docRef = db.collection("Users").document(localUID);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                            user.setUid(localUID); // if the controlling if block is reached, then the document exists, which means localuid = uid stored in firestore
+                            user.setEmail(document.getData().get("email").toString());
+                            user.setName(document.getData().get("name").toString());
+                            user.setParticipatingExperiments((ArrayList<String>) document.getData().get("participatingExperiment"));
+                            user.setOwnedExperiments((ArrayList<String>) document.getData().get("ownedExperiments"));
+                            user.setUsername(document.getData().get("username").toString());
+
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+
+
+        } // if close
+        else{
+            final CollectionReference collectionReference = db.collection("Users");  // connect to users collection
+
+            Map<String,String> data = new HashMap<>();
+
+            ArrayList<String> experiments = new ArrayList();
+
+            Map<String, ArrayList<String>> uExps = new HashMap<>();
+
+
+
+            /*collectionReference
+                    .add(data)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });*/
+
+
+
+            DocumentReference documentReference = db.collection("Users").document();
+            data.put("email","");    // put empty values for now
+            data.put("name","");
+            data.put("uid","");
+            data.put("username","");
+
+            uExps.put("ownedExperiments", experiments);
+            uExps.put("participatingExperiments", experiments);
+
+            documentReference.set(data);
+
+            String uid = documentReference.getId().toString();
+            user.setUid(uid);  // every user should always have a uid which is the uid generated by firestore (unique)
+
+            DocumentReference addID = db.collection("Users").document(uid);
+
+            addID.update("uid", uid);
+            addID.update("participatingExperiments", experiments);
+            addID.update("ownedExperiments", experiments);
+
+
+            SharedPreferences prefSettings = getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("uid", uid);
+
+            // Apply the edits!
+            editor.apply();
+
+
+
+            // Get from the SharedPreferences
+            String localUID = settings.getString("uid", "0");
+
+
+
+        }
+        return user;
+    }
+
     /*
     exListView.setOnLongClickListener(new AdapterView.OnItemLongClickListener() {
         @Override
@@ -164,5 +280,4 @@ public class MainActivity extends AppCompatActivity implements AddExpFragment.On
         }
 
      */
-
 }
