36ca1637c77a110e1f6eb47b9efa333d69c8c4e4
ShafinRez <shafinrezwan123@gmail.com>
2021-03-16 19:13:48 -0600

    exp activity

---
+++ experimently/app/src/main/java/com/example/experimentify/ExperimentActivity.java
@@ -0,0 +1,118 @@
+package com.example.experimentify;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ExperimentActivity extends AppCompatActivity {
+
+    private TextView description;
+    private TextView date;
+    private TextView expType;
+    private TextView location;
+    private Button statsButton;
+    private Button chatButton;
+    private Experiment exp;
+
+    private CardView count;
+    private Button countButton;
+
+    private CardView binomial;
+    private Button passButton;
+    private Button failButton;
+
+    private CardView integer;
+    private EditText intInput;
+
+    private  CardView measure;
+    private EditText measureInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_experiment);
+        description = findViewById(R.id.activ_descrip);
+        date = findViewById(R.id.actiiv_date);
+        expType = findViewById(R.id.activ_expType);
+        location = findViewById(R.id.activ_location);
+        statsButton = findViewById(R.id.stats);
+        chatButton = findViewById(R.id.chat);
+        count = findViewById(R.id.Count);
+        countButton = findViewById(R.id.increaseCount);
+        binomial = findViewById(R.id.Binomial);
+        passButton = findViewById(R.id.pass);
+        failButton = findViewById(R.id.fail);
+        integer = findViewById(R.id.Integer);
+        intInput = findViewById(R.id.integerInput);
+        measure = findViewById(R.id.measurement);
+        measureInput = findViewById(R.id.meaasurementInput);
+
+        description.setText(exp.getDescription());
+        date.setText(exp.getDate());
+        expType.setText(exp.getExperimentId());
+        location.setText(exp.getRegion());
+
+
+        statsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        if (exp.getExpType()=="count"){
+            count.setVisibility(View.VISIBLE);
+            countButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        if(exp.getExpType()=="binomial"){
+            binomial.setVisibility(View.VISIBLE);
+            passButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            failButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        if(exp.getExpType()=="integer"){
+            integer.setVisibility(View.VISIBLE);
+
+        }
+
+        if (exp.getExpType()=="measurement"){
+            measure.setVisibility(View.VISIBLE);
+
+        }
+
+    }
+}
