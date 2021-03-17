258d858e3a1894cd95e98af0ceda1fb387e168d6
ShafinRez <shafinrezwan123@gmail.com>
2021-03-17 14:12:31 -0600

    trial class not complete

--- experimently/app/src/main/java/com/example/experimentify/Trial.java
+++ experimently/app/src/main/java/com/example/experimentify/Trial.java
@@ -4,13 +4,50 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class Trial {
     private int userId;
-    //private Location trialLocation
+    private Location trialLocation;
+    private int count;
+    private int fail;
+    private int pass;
+    private int integerCount;
+    private int measurement;
 
+    public void createCountTrial(){
+        count += 1;
+    }
+
+    public void incrementBinomialFail(){
+        fail += 1;
+    }
+
+    public void incrementBinomialPass(){
+        pass += 1;
+    }
+
+    public int getBinomialTrials(){
+        int total = pass + fail;
+        return total;
+    }
+
+    public void createIntegerCount(){
+        integerCount +=1;
+    }
+
+    /*
+    public int returnIntegerCount(Experiment exp){
+
+
+    }
+    */
+
+    public void createMeasurementTrial(Experiment exp){
+        measurement += 1;
+    }
 
 
 
