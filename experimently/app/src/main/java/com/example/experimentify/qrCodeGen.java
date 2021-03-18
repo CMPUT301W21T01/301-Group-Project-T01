1b9f190c980d52f1563c6f804978c2487586842c
tetelows <tetelows@ualberta.ca>
2021-03-18 12:14:28 -0600

    just need to fix the array part and fix the .jar issue

--- experimently/app/src/main/java/com/example/experimentify/qrCodeGen.java
+++ experimently/app/src/main/java/com/example/experimentify/qrCodeGen.java
@@ -1,57 +1,52 @@
 package com.example.experimentify;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+public class qrCodeGen {
 
-public class qrCodeGen extends AppCompatActivity {
+    public Bitmap builtBitmap;
+    public String encodedID;
+    public int functionality; //n0 = open, 1 = add, 2 = customize
 
-    public class MyQr {
-
-        // Function to create the QR code
-        public void createQR(String data, String path, String charset, Map hashMap, int height, int width) throws WriterException, IOException {
-            BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, width, height);
-            MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
-        }
-
-        // Driver code
-        public void main(String[] args) throws WriterException, IOException, NotFoundException {
-
-            // The data that the QR code will contain
-            String data = "www.geeksforgeeks.org";
-
-            // The path where the image will get saved
-            String path = "demo.png";
-
-            // Encoding charset
-            String charset = "UTF-8";
-
-            Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-
-            hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-
-            // Create the QR code and save
-            // in the specified folder
-            // as a jpg file
-            createQR(data, path, charset, hashMap, 200, 200);
-            System.out.println("QR Code Generated!!! ");
-        }
+    public qrCodeGen(Bitmap builtBitmap, String encodedID, int functionality) {
+        this.builtBitmap = builtBitmap;
+        this.encodedID = encodedID;
+        this.functionality = functionality;
     }
+
+
+    private Bitmap textToImage(String text, int width, int height) throws WriterException, NullPointerException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    width, height, null);
+        } catch (IllegalArgumentException Illegalargumentexception) {
+            return null;
+        }
+
+        int bitMatrixWidth = bitMatrix.getWidth();
+        int bitMatrixHeight = bitMatrix.getHeight();
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        int colorWhite = 0xFFFFFFFF;
+        int colorBlack = 0xFF000000;
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+            for (int x = 0; x < bitMatrixWidth; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? colorBlack : colorWhite;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, width, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+
+
 }
