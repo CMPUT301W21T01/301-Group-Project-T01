a889f3943ef263b55580c04b0f51f6f33371691b
byrontung <btung1@ualberta.ca>
2021-03-26 00:20:09 -0600

    QuestionListAdapter + xml and Question coded, but not tested yet.

--- experimently/app/src/main/java/com/example/experimentify/Question.java
+++ experimently/app/src/main/java/com/example/experimentify/Question.java
@@ -5,22 +5,22 @@ import android.icu.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Question {
-    private String desc;
+    private String description;
     private String uID;
     // Not sure how the date should be stored yet.
     private String date;
     private int numReplies;
     private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-    public Question(String desc, String uID) {
-        this.desc = desc;
+    public Question(String description, String uID) {
+        this.description = description;
         this.uID = uID;
         this.numReplies = 0;
         date = sdf.format(new Date(System.currentTimeMillis()));
     }
 
-    public String getDesc() {
-        return desc;
+    public String getDescription() {
+        return description;
     }
 
     public String getuID() {
--- experimently/app/src/main/java/com/example/experimentify/QuestionListAdapter.java
+++ experimently/app/src/main/java/com/example/experimentify/QuestionListAdapter.java
@@ -18,52 +18,49 @@ import java.util.ArrayList;
   Link: https://eclass.srv.ualberta.ca/pluginfile.php/6713985/mod_resource/content/1/Lab%203%20instructions%20-%20CustomList.pdf
   Code for this class is based on examples from lab 3.
   */
+/**
+ * This class is an ArrayAdapter for Questions.
+ */
+public class QuestionListAdapter extends ArrayAdapter<Question> {
+    private ArrayList<Question> questions;
+    private Context context;
+
+    private TextView descBox;
+    private TextView dateBox;
+    private TextView repliesBox;
+
+    public QuestionListAdapter(Context context, ArrayList<Question> questions) {
+        super(context, 0, questions);
+        this.questions = questions;
+        this.context = context;
+    }
+
     /**
-     * This class is an ArrayAdapter for Questions.
+     * This method sets the TextViews of the question adapter.
+     * @param question Question whose data is displayed by the question adapter
      */
-    public class QuestionListAdapter extends ArrayAdapter<Question> {
-        private ArrayList<Question> questions;
-        private Context context;
+    public void setUi(Question question) {
+        descBox.setText(context.getResources().getString(R.string.description_header) + question.getDescription());
+        dateBox.setText(context.getResources().getString(R.string.date_header) + question.getDate());
+        repliesBox.setText(context.getResources().getString(R.string.replies_header) + question.getNumReplies());
+    }
 
-        private TextView descBox;
-        private TextView regionBox;
-        private TextView dateBox;
-        private TextView repliesBox;
+    //TODO add javadocs comment
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
 
-        public QuestionListAdapter(Context context, ArrayList<Question> questions) {
-            super(context, 0, questions);
-            this.questions = questions;
-            this.context = context;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.question_list_content, parent, false);
         }
 
-        /**
-         * This method sets the TextViews of the question adapter.
-         * @param question Experiment whose data is displayed by the experiment adapter
-         */
-        public void setUi(Question question) {
-            descBox.setText(context.getResources().getString(R.string.description_header) + question.getDescription());
-            regionBox.setText(context.getResources().getString(R.string.region_header) + question.getRegion());
-            dateBox.setText(context.getResources().getString(R.string.date_header) + exp.getDate());
-        }
+        Question question = questions.get(position);
 
-        //TODO add javadocs comment
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            View view = convertView;
+        descBox = view.findViewById(R.id.generalQuestionTitle);
+        dateBox = view.findViewById(R.id.generalQuestionDate);
+        repliesBox = view.findViewById(R.id.generalQuestionResponses);
 
-            if (view == null) {
-                view = LayoutInflater.from(context).inflate(R.layout.experiment_list_content, parent, false);
-            }
+        setUi(question);
 
-            Experiment experiment = experiments.get(position);
-
-            //TODO show status and owner of experiment in list
-            descBox = view.findViewById(R.id.generalExDescription);
-            regionBox = view.findViewById(R.id.generalExLocation);
-            dateBox = view.findViewById(R.id.generalExDate);
-
-            setUi(experiment);
-
-            return view;
-        }
+        return view;
     }
 }
