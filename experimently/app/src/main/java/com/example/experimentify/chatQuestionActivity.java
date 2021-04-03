1c7fa928ca47ec59ea2c9752b31a8768412a6a1d
tetelows <tetelows@ualberta.ca>
2021-04-02 23:37:57 -0600

    i really didnt get far, just tried to implemnet the funcionality behind other than DB, im consued on how i use the controller to get get and populate the list view. I will be up early working tmr so i so i doubt u will read this

--- experimently/app/src/main/java/com/example/experimentify/chatQuestionActivity.java
+++ experimently/app/src/main/java/com/example/experimentify/chatQuestionActivity.java
@@ -1,16 +1,20 @@
 package com.example.experimentify;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class chatQuestionActivity extends AppCompatActivity {
 
     private EditText userQuestionInput;
+    private ListView questionsList;
     private Button questionEnter;
     private SharedPreferences settings;
     public static final String PREFS_NAME = "PrefsFile";
@@ -22,15 +26,25 @@ public class chatQuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat_question);
         userQuestionInput = findViewById(R.id.questionInput);
         questionEnter = findViewById(R.id.questionInputButton);
+        questionsList = findViewById(R.id.questionList);
 
         questionEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO: Add question to DB
+
                 userQuestionInput.getText();
-                settings = getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
+                //settings = getApplicationContext().getSharedPreferences(PREFS_NAME, 0);
+            }
+        });
 
 
+        questionsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+                Intent intent = new Intent(chatQuestionActivity.this, chatAnswerActivity.class);
+                //intent.putExtra("qid", queuestionID);
+                startActivity(intent);
             }
         });
 
